8c0dfdb62f6600d1805028e1b74e964339f9977f
Rehnyk <hnykova.renata@gmail.com>
2023-10-18 13:24:50 +0300

    3.3 show single person

--- part3/1-phonebook/index.js
+++ part3/1-phonebook/index.js
@@ -36,6 +36,20 @@ app.get('/info', (request, response) => {
     response.send('<p>Phonebook has info for X people</p>')
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
