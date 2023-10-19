26fe9d52d3a73f8315d0cc6bea5c41e5c5406831
Rehnyk <hnykova.renata@gmail.com>
2023-10-19 16:05:47 +0300

    uibuild

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -15,6 +15,7 @@ morgan.token('postData', (req, res) => {
 app.use(cors());
 app.use(morgan(':method :url :status :response-time ms :postData'));
 app.use(express.json());
+app.use(express.static('dist'))
 
 
 let persons = [
