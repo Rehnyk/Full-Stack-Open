a7ba6197c2ea9c3f2973f57ac5a54fb591fba4cb
Rehnyk <hnykova.renata@gmail.com>
2023-10-19 20:14:15 +0300

    3.14 use database for phonebook, create separate model for database

--- notes-app/backend/index.js
+++ notes-app/backend/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note.js')
+
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -19,72 +22,35 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy123123123",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript.......",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
 
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
-
-    response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -93,6 +59,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
+
 
 app.use(unknownEndpoint)
 
