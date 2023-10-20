9b7be7e307247b08265dff30b558f1c9cb06106b
Rehnyk <hnykova.renata@gmail.com>
2023-10-20 14:32:44 +0300

    error handling

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -14,35 +14,53 @@ morgan.token('postData', (req, res) => {
     }
 });
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+}
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+
+    next(error);
+};
+
+app.use(express.json());
 app.use(cors());
 app.use(morgan(':method :url :status :response-time ms :postData'));
-app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
-
-
+// Show all people
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(person => {
         response.json(person)
-    })
+    });
 });
 
-
-app.get('/api/persons/:id', (request, response) => {
+// Show single person
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
+        if (person) {
+            response.json(person);
+        } else {
+            response.status(404).end();
+        }
     })
+        .catch(error => next(error))
 });
 
-/*
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end();
+// Delete a person
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 });
-*/
 
+// Add a new person
 app.post('/api/persons', (request, response) => {
     const body = request.body;
 
@@ -61,6 +79,26 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 });
+
+// Change number of a person
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
