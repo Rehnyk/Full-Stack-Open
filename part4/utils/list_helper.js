9254b440cd64247dcc269c9dc75ffda21189ab13
Rehnyk <hnykova.renata@gmail.com>
2023-10-23 18:58:24 +0300

    4.5 test for favorite blog

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,8 +10,30 @@ const totalLikes = (posts) => {
     return sum;
 };
 
+const favoriteBlog = (blogs) => {
+
+    if (blogs.length === 0) {
+        return null;
+    }
+
+    let maxLikes = -1;
+    let favorite = null;
+
+    blogs.forEach(b => {
+        if(b.likes > maxLikes) {
+            maxLikes = b.likes;
+            favorite = {
+                title: b.title,
+                author: b.author,
+                likes: b.likes
+            };
+        }
+    });
+    return favorite;
+};
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
