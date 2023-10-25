53afb3c6f0d65fb645acd8c4fd8c05281bf950fd
Rehnyk <hnykova.renata@gmail.com>
2023-10-25 16:13:10 +0300

    refactor tests into describe blocks

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -8,92 +8,100 @@ const Blog = require('../models/blog.js');
 
 beforeEach(async () => {
     await Blog.deleteMany({});
-
-    const blogObjects = helper.initialBlogs
-        .map(blog => new Blog(blog));
-    const promiseArray = blogObjects.map(blog => blog.save());
-    await Promise.all(promiseArray);
+    await Blog.insertMany(helper.initialBlogs);
 });
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/);
-});
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs');
+describe('when there are notes saved initially', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/);
+    });
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length);
-});
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs');
 
-test('unique identifier property is named "id" ', async () => {
-    const response = await api.get('/api/blogs');
-
-    expect(response.body[0].id).toBeDefined();
+        expect(response.body).toHaveLength(helper.initialBlogs.length);
+    });
 });
 
-test('a valid blog can be added ', async () => {
-    const newBlog =  {
-        title: 'Coding',
-        author: 'Tom',
-        url: 'coding.com',
-        likes: 7
-    };
+describe ('checking id properties of a blog', () => {
+    test('unique identifier property is named "id" ', async () => {
+        const response = await api.get('/api/blogs');
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/);
+        expect(response.body[0].id).toBeDefined();
+    });
 
-    const blogsAtEnd = await helper.blogsInDb();
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    test('likes default to 0, if property is missing from request', async () => {
+        const newBlog = {
+            title: 'Sports',
+            author: 'Jake',
+            url: 'sports.com'
+        };
 
-    expect(blogsAtEnd).toContainEqual(expect.objectContaining(newBlog));
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201);
+
+        const savedBlog = response.body;
+        expect(savedBlog.likes).toBe(0);
+    });
 });
 
-test('likes default to 0, if property is missing from request', async () => {
-    const newBlog = {
-        title: 'Sports',
-        author: 'Jake',
-        url: 'sports.com'
-    };
+describe('adding new note', () => {
+    test('a valid blog can be added ', async () => {
+        const newBlog =  {
+            title: 'Coding',
+            author: 'Tom',
+            url: 'coding.com',
+            likes: 7
+        };
 
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201);
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/);
 
-    const savedBlog = response.body;
-    expect(savedBlog.likes).toBe(0);
+        const blogsAtEnd = await helper.blogsInDb();
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+        expect(blogsAtEnd).toContainEqual(expect.objectContaining(newBlog));
+    });
+
+    test('if title or url is missing from request, respond with 400 status code', async () => {
+        const noTitle = {
+            author: 'Jake',
+            url: 'sports.com',
+            likes: 8
+        };
+
+        const noUrl = {
+            title: 'Sports',
+            author: 'Jake',
+            likes: 8
+        };
+
+        await api
+            .post('/api/blogs')
+            .send(noTitle)
+            .expect(400);
+
+        await api
+            .post('/api/blogs')
+            .send(noUrl)
+            .expect(400);
+    });
 });
 
-test('title is missing from request', async () => {
-    const newBlog = {
-        author: 'Jake',
-        url: 'sports.com',
-        likes: 8
-    };
+describe('deleting a note', () => {});
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400);
-});
+describe('updating a note', () => {});
 
-test('url is missing from request', async () => {
-    const newBlog = {
-        title: 'Sports',
-        author: 'Jake',
-        likes: 8
-    };
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400);
-});
+
 
 afterAll(async () => {
     await mongoose.connection.close();
