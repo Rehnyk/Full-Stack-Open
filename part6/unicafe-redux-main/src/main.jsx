0b5254277e68b9eec217537eafcfa157bae8e2b8
Rehnyk <hnykova.renata@gmail.com>
2024-01-22 14:38:51 +0200

    6.2

--- part6/unicafe-redux-main/src/main.jsx
+++ part6/unicafe-redux-main/src/main.jsx
@@ -37,6 +37,7 @@ const App = () => {
       <button onClick={ok}>ok</button>
       <button onClick={bad}>bad</button>
       <button onClick={zero}>reset stats</button>
+
       <div>good {store.getState().good}</div>
       <div>ok {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
