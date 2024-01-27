72ce285c98f0c471fc408c865438a7021a222bf0
Rehnyk <hnykova.renata@gmail.com>
2024-01-27 12:01:06 +0200

    6.12

---
+++ part6/anecdotes/src/components/Notification.jsx
@@ -0,0 +1,20 @@
+//import notificationReducer, { setNotification } from '../reducers/notificationReducer.js'
+import { useSelector } from 'react-redux'
+const Notification = () => {
+ // const dispatch = useDispatch()
+  const notification = useSelector(state => state)
+
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  return (
+    <div style={style}>
+      {notification}
+    </div>
+  )
+}
+
+export default Notification
